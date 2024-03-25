f085d5f3c11057bab4ab0fa9b9a482c4311b62ee
YehorVahin <davyjohns322@gmail.com>
2024-03-25 17:23:02 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,6 @@
 'use strict';
 
-function getElementWidth(content, padding, border){
-return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2
+function checkForSpam(message){
+const lowerCaseMessage = message.toLowerCase();
+return lowerCaseMessage.includes(`spam`) || lowerCaseMessage.includes(`sale`);
 };
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
