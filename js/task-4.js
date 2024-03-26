b5775311320103daa27d717d48c2ed4b3caef8b5
YehorVahin <davyjohns322@gmail.com>
2024-03-26 11:51:17 +0200

    1

--- js/task-4.js
+++ js/task-4.js
@@ -17,3 +17,7 @@ function getShippingCost(country) {
 }
 console.log(getShippingCost("Germany"));
 console.log(getShippingCost("China"));
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
