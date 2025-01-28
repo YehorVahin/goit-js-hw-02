3d354ae1b971b835ce054598f51f64d9be7fd78c
YehorVahin <davyjohns322@gmail.com>
2025-01-28 22:03:06 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+$(function () {
+	$(".slider-inner").slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		
+	});
+});
+$(function () {
+	$(".about-slider-inner").slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		
+	});
+});
